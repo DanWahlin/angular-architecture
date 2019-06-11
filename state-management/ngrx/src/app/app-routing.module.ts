ea80dab050831d7c51dff4721fb7104d55daeaf7
Dan Wahlin <dwahlin@xmlforasp.net>
2019-06-11 00:44:28 -0400

    Update to v8

--- state-management/ngrx/src/app/app-routing.module.ts
+++ state-management/ngrx/src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'customers' },
-  { path: 'customers', loadChildren: 'app/customers/customers.module#CustomersModule' },
-  { path: 'orders/:id', loadChildren: 'app/orders/orders.module#OrdersModule' }
+  { path: 'customers', loadChildren: () => import('app/customers/customers.module').then(m => m.CustomersModule) },
+  { path: 'orders/:id', loadChildren: () => import('app/orders/orders.module').then(m => m.OrdersModule) }
 ];
 
 @NgModule({
