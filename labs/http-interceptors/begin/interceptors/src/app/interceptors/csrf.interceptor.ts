0c2da82d103203060f141c6543c86bfef9b03342
John Papa <john@johnpapa.net>
2020-03-29 21:42:50 -0400

    lab init

--- labs/http-interceptors/begin/interceptors/src/app/interceptors/csrf.interceptor.ts
+++ labs/http-interceptors/begin/interceptors/src/app/interceptors/csrf.interceptor.ts
@@ -5,9 +5,6 @@ import { Observable } from 'rxjs';
 @Injectable({ providedIn: 'root' })
 export class CSRFInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    // Get your CSRF from your cookie
-    const clonedReq = req.clone({ setHeaders: { 'x-csrf-token': 'your-csrf-token-goes-here' } });
-    console.log(`HTTP: Adding CSRF`);
-    return next.handle(clonedReq);
+    //
   }
 }
--- labs/http-interceptors/end/interceptors/src/app/interceptors/index.ts
+++ labs/http-interceptors/end/interceptors/src/app/interceptors/index.ts
@@ -15,40 +15,6 @@ const multi = true;
 export const httpInterceptorProviders = [
   { provide, useClass: AuthInterceptor, multi },
   { provide, useClass: CSRFInterceptor, multi },
-  { provide, useClass: LogResponseTimeInterceptor, multi },
   { provide, useClass: LogHeadersInterceptor, multi }
+  { provide, useClass: LogResponseTimeInterceptor, multi },
 ];
-
-/**
- * https://angular.io/guide/http#http-interceptors
- *
- * Why you care?
- * Have you ever needed to add headers to all or a subset of http requests? Transform the response? Log specific requests?
- * Without interception, developers would have to implement these tasks explicitly for each HttpClient method call.
- *
- * What is an interceptor?
- * HTTP Interception is a major feature of @angular/common/http. With interception, you declare interceptors that inspect and transform HTTP requests from your application to the server. The same interceptors may also inspect and transform the server's responses on their way back to the application. Multiple interceptors form a forward-and-backward chain of request/response handlers.
- * Interceptors can perform a variety of implicit tasks, from authentication to logging, in a routine, standard way, for every HTTP request/response.
- *
- * Providing Interceptors
- * Because interceptors are (optional) dependencies of the HttpClient service, you must provide them in the same injector (or a parent of the injector) that provides HttpClient. Interceptors provided after DI creates the HttpClient are ignored.
- * Use a barrel and export an array of the interceptors.
- *
- * Modifying Requests
- *  Although interceptors are capable of mutating requests and responses, the HttpRequest and HttpResponse instance properties are readonly, rendering them largely immutable.
- *  The request is immutable ... you must clone it.
- * Most interceptors transform the outgoing request before passing it to the next interceptor in the chain, by calling next.handle(transformedReq). An interceptor may transform the response event stream as well, by applying additional RxJS operators on the stream returned by next.handle().
- *
- * Modifing Responses
- * After you return stream via next.handle(), you can pipe the stream's response. Use RxJS operators to transform or do what you will to it.
- * The response is immutable ... you must clone it.
- *
- * What is multi true?
- * The multi: true option is a required setting that tells Angular that HTTP_INTERCEPTORS is a token for a multiprovider that injects an array of values, rather than a single value.
- *
- * Order is important
- * Angular applies interceptors in the order that you provide them. If you provide interceptors A, then B, then C, requests will flow in A->B->C and responses will flow out C->B->A.
-
-
-
- */
