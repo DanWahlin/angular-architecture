f30a4bee11b44004df15015c89fef3889b699b5f
John Papa <john@johnpapa.net>
2020-06-29 08:26:16 -0400

    lint

--- labs/http-interceptors/begin/src/app/interceptors/index.ts
+++ labs/http-interceptors/begin/src/app/interceptors/index.ts
@@ -1,13 +1,13 @@
-import { HTTP_INTERCEPTORS } from "@angular/common/http";
-import { AuthInterceptor } from "./auth.interceptor";
-import { CSRFInterceptor } from "./csrf.interceptor";
-import { LogResponseTimeInterceptor } from "./log-response.interceptor";
-import { LogHeadersInterceptor } from "./log-headers.interceptor";
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor } from './auth.interceptor';
+import { CSRFInterceptor } from './csrf.interceptor';
+import { LogResponseTimeInterceptor } from './log-response.interceptor';
+import { LogHeadersInterceptor } from './log-headers.interceptor';
 
-export * from "./auth.interceptor";
-export * from "./csrf.interceptor";
-export * from "./log-headers.interceptor";
-export * from "./log-response.interceptor";
+export * from './auth.interceptor';
+export * from './csrf.interceptor';
+export * from './log-headers.interceptor';
+export * from './log-response.interceptor';
 
 const provide = HTTP_INTERCEPTORS;
 const multi = true;
@@ -15,6 +15,6 @@ const multi = true;
 export const httpInterceptorProviders = [
   { provide, useClass: AuthInterceptor, multi },
   // CSRF interceptor can go here
-  { provide, useClass: LogHeadersInterceptor, multi }
+  { provide, useClass: LogHeadersInterceptor, multi },
   // Response interceptor can go here
 ];
