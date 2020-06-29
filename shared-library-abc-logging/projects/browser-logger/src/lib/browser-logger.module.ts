ccb782101dad21313465175d26531d5d05d558a4
John Papa <john@johnpapa.net>
2020-06-29 07:57:05 -0400

    ng up for abc logging

--- shared-library-abc-logging/projects/browser-logger/src/lib/browser-logger.module.ts
+++ shared-library-abc-logging/projects/browser-logger/src/lib/browser-logger.module.ts
@@ -2,7 +2,7 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 
 @NgModule()
 export class BrowserLoggerModule {
-  public static forRoot(environment: any): ModuleWithProviders {
+  public static forRoot(environment: any): ModuleWithProviders<BrowserLoggerModule> {
     return {
       ngModule: BrowserLoggerModule,
       providers: [
