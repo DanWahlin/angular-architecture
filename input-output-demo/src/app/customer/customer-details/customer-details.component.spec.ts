901477bbb8d7284c0b8e6bcdb3d80eb2cffc47d3
Dan Wahlin <danwahlin@iMac-Office.local>
2020-11-14 13:38:28 -0700

    Update input output demo

--- input-output-demo/src/app/customer/customer-details/customer-details.component.spec.ts
+++ input-output-demo/src/app/customer/customer-details/customer-details.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { CustomerDetailsComponent } from './customer-details.component';
 
@@ -6,7 +6,7 @@ describe('CustomerDetailsComponent', () => {
   let component: CustomerDetailsComponent;
   let fixture: ComponentFixture<CustomerDetailsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ CustomerDetailsComponent ]
     })
