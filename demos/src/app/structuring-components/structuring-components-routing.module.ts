a957e28745df1016e5bc37295d71a13f0a8af0ea
Dan Wahlin <danwahlin@iMac-Office.local>
2020-11-14 13:31:36 -0700

    Update main demos

--- demos/src/app/structuring-components/structuring-components-routing.module.ts
+++ demos/src/app/structuring-components/structuring-components-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
+  imports: [ RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' }) ],
   exports: [ RouterModule ]
 })
 export class StructuringComponentsRoutingModule {
--- demos/src/app/view-model/view-model.module.ts
+++ demos/src/app/view-model/view-model.module.ts
@@ -22,7 +22,7 @@ const routes: Routes = [
   }
 ];
 @NgModule({
-  imports: [CommonModule, RouterModule.forRoot(routes)],
+  imports: [CommonModule, RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
   declarations: [CustomersOrdersShellComponent]
 })
 export class ViewModelModule {}
