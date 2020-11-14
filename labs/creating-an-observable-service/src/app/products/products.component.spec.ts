37349bcbf6c19504712ef23ae731e3853c6fef93
Dan Wahlin <danwahlin@iMac-Office.local>
2020-11-14 09:24:16 -0700

    Update creating observable service to 11

--- labs/creating-an-observable-service/src/app/products/products.component.spec.ts
+++ labs/creating-an-observable-service/src/app/products/products.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ProductsComponent } from './products.component';
 
@@ -6,7 +6,7 @@ describe('ProductsComponent', () => {
   let component: ProductsComponent;
   let fixture: ComponentFixture<ProductsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ProductsComponent ]
     })
