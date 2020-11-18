11f5cf671f543071c93aff777b7d3447bbb5618d
John Papa <john@johnpapa.net>
2020-11-18 14:01:06 -0500

    preload inject

--- labs/routing-guards-and-preload-strategies/end/src/app/core/opt-in-preload-strategy.ts
+++ labs/routing-guards-and-preload-strategies/end/src/app/core/opt-in-preload-strategy.ts
@@ -1,8 +1,10 @@
+import { Injectable } from "@angular/core";
 import { PreloadingStrategy, Route } from "@angular/router";
 import { Observable, of } from "rxjs";
 
+@Injectable({ providedIn: 'root' })
 export class PreloadSelectedModulesList implements PreloadingStrategy {
   preload(route: Route, load: Function): Observable<any> {
-    return route.data && route.data["preload"] ? load() : of(null);
+    return route.data && route.data['preload'] ? load() : of(null);
   }
 }
