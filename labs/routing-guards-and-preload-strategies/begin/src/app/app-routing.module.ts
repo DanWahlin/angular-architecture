dea9e4a665e908e07998d91235cede9ea6f91d77
John Papa <john@johnpapa.net>
2020-11-18 13:52:31 -0500

    fix

--- labs/routing-guards-and-preload-strategies/begin/src/app/app-routing.module.ts
+++ labs/routing-guards-and-preload-strategies/begin/src/app/app-routing.module.ts
@@ -1,32 +1,35 @@
-import { NgModule } from "@angular/core";
-import { Routes, RouterModule, PreloadAllModules } from "@angular/router";
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import {
   PreloadSelectedModulesList,
-  NetworkAwarePreloadStrategy
-} from "./core";
+  NetworkAwarePreloadStrategy,
+} from './core';
 
 const routes: Routes = [
-  { path: "", pathMatch: "full", redirectTo: "heroes" },
+  { path: '', pathMatch: 'full', redirectTo: 'heroes' },
   {
-    path: "heroes",
-    loadChildren: () => import('./heroes/heroes.module').then(m => m.HeroesModule)
+    path: 'heroes',
+    loadChildren: () =>
+      import('./heroes/heroes.module').then((m) => m.HeroesModule),
   },
   {
-    path: "villains",
-    loadChildren: () => import('./villains/villains.module').then(m => m.VillainsModule)
-  }
+    path: 'villains',
+    loadChildren: () =>
+      import('./villains/villains.module').then((m) => m.VillainsModule),
+  },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes
-// Uncomment the line of code below. Put the Preload Strategy here
-// PreloadAllModules
-// PreloadSelectedModulesList,
-// NetworkAwarePreloadStrategy
-// { preloadStrategy: PreloadAllModules }
-, { relativeLinkResolution: 'legacy' })
+    RouterModule.forRoot(routes, {
+      // Uncomment the line of code below. Put the Preload Strategy here
+      // preloadingStrategy: PreloadAllModules,
+      // preloadingStrategy: PreloadSelectedModulesList,
+      // preloadingStrategy: NetworkAwarePreloadStrategy,
+      // preloadingStrategy: PreloadAllModules,
+      relativeLinkResolution: 'legacy',
+    }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
