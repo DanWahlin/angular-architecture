8e7b9e93acc9f23dc2f5c23ac7d9ca6039028041
John Papa <john@johnpapa.net>
2023-05-30 01:15:48 -0400

    v16

--- labs/view-models/begin/src/app/app-routing.module.ts
+++ labs/view-models/begin/src/app/app-routing.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/customers' },
-  { path: 'customers', loadChildren: () => import('./customers/customers.module').then(m => m.CustomersModule)   }
+  {
+    path: 'customers',
+    loadChildren: () =>
+      import('./customers/customers.module').then((m) => m.CustomersModule),
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, {})],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
