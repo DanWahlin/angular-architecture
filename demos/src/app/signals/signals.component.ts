712e45afe33ec8b93bc4cabc567baf8316e89291
Dan Wahlin <you@example.com>
2023-06-08 23:05:46 -0700

    Update edit feature for signals

--- demos/src/app/signals/signals.component.ts
+++ demos/src/app/signals/signals.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, inject, signal } from '@angular/core';
+import { Component, Injector, computed, effect, inject, signal } from '@angular/core';
 import { Customer } from '../shared/interfaces';
 import { DataService } from '../core/services/data.service';
 import { delay } from 'rxjs';
@@ -9,42 +9,73 @@ import { delay } from 'rxjs';
   styleUrls: ['./signals.component.css']
 })
 export class SignalsComponent {
-  customers: Customer[] = [];
-  editingCustomer = signal<Customer | null>(null);
-
-  // Define a signal for the loading state
+  customers = signal<Customer[]>([]);
+  selectedCustomer = signal<Customer>({} as Customer);
   loading = signal(true);
-
-  // Define a signal for the search query
-  searchQuerySignal = signal('');
+  searchQuery = signal('');
+  dataService = inject(DataService);
+  // This is a bit "meta"
+  injector = inject(Injector);
 
   // Define a computed value for the filtered customers
   filteredCustomers = computed(() => {
-    return this.customers.filter((customer) =>
-      customer.name.toLowerCase().includes(this.searchQuerySignal().toLowerCase())
+    return this.customers().filter((customer) =>
+      customer.name.toLowerCase().includes(this.searchQuery().toLowerCase())
     );
   });
 
-  dataService = inject(DataService);
-
   ngOnInit() {
+    this.initializeLogging();
     this.dataService.getCustomers().pipe(
-        delay(2000) // simulate delay for our loading signal
+        delay(1000) // simulate delay for our loading signal
       ).subscribe(custs => {
       this.loading.set(false);
-      this.customers = custs;
+      this.customers.set(custs);
     });
   }
 
+  initializeLogging(): void {
+    // Called as signals change
+    // Effects used for very specific use cases: https://angular.io/guide/signals#effects
+    effect(() => {
+      console.log(`Selected customer name: ${this.selectedCustomer().name}`);
+    }, {injector: this.injector});
+  }
+
+  onSearchQuery(event: Event) {
+    this.searchQuery.set((event.target as HTMLInputElement).value);
+  }
+
+  onCustomerNameChange(event: Event) {
+    this.selectedCustomer.mutate(value => value.name = (event.target as HTMLInputElement).value);
+  }
+
   editCustomer(customer: Customer) {
-    this.editingCustomer.set(customer);
+    this.selectedCustomer.set(customer);
   }
 
   isEditingCustomer(customer: Customer) {
-    return this.editingCustomer() === customer;
+    return this.selectedCustomer() === customer;
+  }
+
+  save() {
+    // mutate this.customers with the value of this.editingCustomer()
+    this.customers.mutate(value => {
+      const index = value.findIndex(c => c.id === this.selectedCustomer().id);
+      value[index] = this.selectedCustomer();
+    });
+    this.resetEditing();
+  }
+
+  cancel() {
+    this.resetEditing();
   }
 
   isLoading() {
     return this.loading();
+  }
+
+  resetEditing() {
+    this.selectedCustomer.set({} as Customer);
   }
 }
