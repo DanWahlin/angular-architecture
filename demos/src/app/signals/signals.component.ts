09f5b754889a9eb5b5f0fee5199fcf4c49f7d424
John Papa <john@johnpapa.net>
2024-03-17 15:11:11 -0400

    Refactor SignalsComponent HTML and CSS files

--- demos/src/app/signals/signals.component.ts
+++ demos/src/app/signals/signals.component.ts
@@ -6,8 +6,56 @@ import { delay } from 'rxjs';
 @Component({
   selector: 'app-signals',
   standalone: true,
-  templateUrl: './signals.component.html',
-  styleUrls: ['./signals.component.css']
+  template: `
+    @if (isLoading()) { Loading... } @else {
+    <input
+      type="text"
+      (input)="onSearchQuery($event)"
+      title="Search Query"
+      placeholder="Search query"
+      [disabled]="!!selectedCustomer()"
+    />
+
+    @for(customer of filteredCustomers(); track customer.id) {
+    <div [class.highlight]="isEditingCustomer(customer)">
+      @if(isEditingCustomer(customer)) {
+
+      <!-- Customer editing dialog -->
+      <input
+        type="text"
+        [value]="selectedCustomer()!.name"
+        (input)="onCustomerNameChange($event)"
+        title="customer name"
+      />
+      <button (click)="save()">Save</button>
+      <button (click)="cancel()">Cancel</button>
+
+      } @else {
+
+      {{ customer.name }}
+      <button (click)="editCustomer(customer)">Edit</button>
+      }
+    </div>
+    } }
+  `,
+  styles: `
+    div {
+      margin-top: 20px;
+    }
+
+    button {
+        background-color: #e5e5e5;
+        border: 0;
+        margin-left: 10px;
+    }
+
+    .highlight {
+      background-color: lightblue;
+      font-size: 110%;
+      padding: 1rem;
+      width: 30%;
+    }
+  `,
 })
 export class SignalsComponent {
   customers = signal<Customer[]>([]);
@@ -18,7 +66,7 @@ export class SignalsComponent {
   /** Computed value for the filtered customers.
    * Updates when customer list or search query change. */
   filteredCustomers = computed(() =>
-    this.customers().filter(customer =>
+    this.customers().filter((customer) =>
       customer.name.toLowerCase().includes(this.searchQuery().toLowerCase())
     )
   );
@@ -33,15 +81,18 @@ export class SignalsComponent {
     });
 
     effect(() => {
-      const query = this.searchQuery()
+      const query = this.searchQuery();
       console.log(`Search query: ${query}`);
     });
   }
 
   ngOnInit() {
-    this.dataService.getCustomers().pipe(
+    this.dataService
+      .getCustomers()
+      .pipe(
         delay(1000) // simulate delay for our loading signal
-      ).subscribe(customers => {
+      )
+      .subscribe((customers) => {
         this.customers.set(customers);
         this.isLoading.set(false);
       });
@@ -71,8 +122,8 @@ export class SignalsComponent {
   save() {
     const customer = this.selectedCustomer();
     if (customer) {
-      this.customers.update(customers => {
-        const index = customers.findIndex(c => c.id === customer.id);
+      this.customers.update((customers) => {
+        const index = customers.findIndex((c) => c.id === customer.id);
         customers[index] = customer;
         return customers;
       });
@@ -94,14 +145,16 @@ export class SignalsComponent {
     const name = (event.target as HTMLInputElement).value;
 
     // Immutable update of the selected customer
-    this.selectedCustomer.update(customer => ({ ...customer!, name }));
+    this.selectedCustomer.update((customer) => ({ ...customer!, name }));
   }
 
   save_immutable() {
     const customer = this.selectedCustomer();
     if (customer) {
       // Immutable update of the customers array
-      this.customers.update(customers => customers.map(c => c.id === customer.id ? customer : c));
+      this.customers.update((customers) =>
+        customers.map((c) => (c.id === customer.id ? customer : c))
+      );
       this.resetEditing();
     }
   }
