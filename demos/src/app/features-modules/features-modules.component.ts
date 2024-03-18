1f0f27d65149112e7ac744e626db4e67d8486344
John Papa <john@johnpapa.net>
2024-03-17 21:44:51 -0400

    SFC fo feature moules demo. didnt standalone these as they are intended to show modules

--- demos/src/app/features-modules/features-modules.component.ts
+++ demos/src/app/features-modules/features-modules.component.ts
@@ -2,14 +2,28 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-features-modules',
-  templateUrl: './features-modules.component.html',
-  styleUrls: [ './features-modules.component.css' ]
+  template: `
+    <h1>Features and Modules</h1>
+    Each feature should have it's own module and routing module. Click one of
+    the links below to lazy load the feature module.
+    <br /><br />
+    <a routerLink="customers">Customers Feature</a>&nbsp;&nbsp;
+    <a routerLink="orders">Orders Feature</a>
+    <br /><br />
+    <img src="/assets/features-modules.png" width="600px" />
+    <br /><br />
+    <router-outlet></router-outlet>
+  `,
+  styles: `
+  a {
+    font-weight: bold;
+    font-size: 14pt;
+    text-decoration: underline;
+}
+  `,
 })
 export class FeaturesModulesComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
